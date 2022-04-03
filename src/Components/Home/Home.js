70eb5800d59b7d49a19f4a92ea692505786637d2
hasanabil <mahmudul.anabil2862@gmail.com>
2022-04-04 02:59:01 +0600

    product added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import studio from '../../assets/studio.png'
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <div className='top-container'>
+                <img src={studio} alt="" />
+                <div className='docs-container'>
+                    <h3>Looking for a new computer?</h3>
+                    <h1>Check out the new Mac Studio</h1>
+                    <p>The worlds most powerful compact computer. It's ever more powerful than the highest end Mac Pro</p>
+                    <a target="_blank " href="https://www.apple.com/mac-studio/">Buy Now</a>
+                </div>
+            </div>
+            <div className='reviews-container'>
+                <h2>Reviews</h2>
+            </div>
         </div>
     );
 };
